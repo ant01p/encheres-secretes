defff72f6de6a1793b9daa5e9703c2324657f137
ant01p <ponvienne.antoine@gmail.com>
2026-04-20 16:21:29 +0200

    Bonus-pbmigr-finalpriceNotNull

--- app/src/Controller/ItemController.php
+++ app/src/Controller/ItemController.php
@@ -80,7 +80,7 @@ final class ItemController extends AbstractController
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
-        // si unpublished → publier
+        // si unpublished -> publier
         if ($item->getStatus() === Item::STATUS_UNPUBLISHED) {
             $item->setStatus(Item::STATUS_PUBLISHED);
         }
@@ -88,7 +88,7 @@ final class ItemController extends AbstractController
         // si published
         elseif ($item->getStatus() === Item::STATUS_PUBLISHED) {
 
-            // 👉 s'il y a des offres → fermer + choisir gagnant
+            // et s'il y a des offres -> clotuere et choisir gagnant
             if (count($item->getOffers()) > 0) {
 
                 $bestOffer = null;
@@ -104,7 +104,7 @@ final class ItemController extends AbstractController
                 $item->setFinalPrice($bestOffer->getAmount());
 
             } else {
-                // 👉 sinon → repasser en unpublished
+                // sinon repasser en unpublished
                 $item->setStatus(Item::STATUS_UNPUBLISHED);
             }
         }
@@ -127,5 +127,96 @@ final class ItemController extends AbstractController
             'items' => $items,
             'categories' => $categories,
         ]);
+    }
+
+    #[Route('/admin/item/new', name: 'app_admin_item_new')]
+    public function new(Request $request, EntityManagerInterface $em, CategoryRepository $categoryRepository): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        if ($request->isMethod('POST')) {
+
+            $title = $request->request->get('title');
+            $description = $request->request->get('description');
+            $price = $request->request->get('startingPrice');
+            $categoryIds = $request->request->all('categories');
+
+            // vérif
+            if (!$title || !$description || !$price) {
+                $this->addFlash('error', 'Tous les champs sont obligatoires');
+                return $this->redirectToRoute('app_admin_item_new');
+            }
+
+            $item = new Item();
+            $item->setTitle($title);
+            $item->setDescription($description);
+            $item->setStartingPrice((int)$price);
+            $item->setStatus('unpublished');
+
+            foreach ($categoryIds as $categoryId) {
+            $category = $categoryRepository->find($categoryId);
+
+            if ($category) {
+                $item->addCategory($category);
+            }
+        }
+            $em->persist($item);
+            $em->flush();
+
+            return $this->redirectToRoute('app_item');
+        }
+
+        return $this->render('item/item_form.html.twig', [
+            'title' => 'Ajouter un objet',
+            'item' => null,
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/admin/item/{id}/edit', name: 'app_admin_item_edit')]
+    public function edit(Item $item, Request $request, EntityManagerInterface $em, CategoryRepository $categoryRepository): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        if ($request->isMethod('POST')) {
+            $item->setTitle($request->request->get('title'));
+            $item->setDescription($request->request->get('description'));
+            $item->setStartingPrice((int) $request->request->get('startingPrice'));
+
+            foreach ($item->getCategories() as $category) {
+            $item->removeCategory($category);
+        }
+
+            $categoryIds = $request->request->all('categories');
+
+            foreach ($categoryIds as $categoryId) {
+                $category = $categoryRepository->find($categoryId);
+
+                if ($category) {
+                    $item->addCategory($category);
+                }
+            }
+
+                $em->flush();
+
+                return $this->redirectToRoute('app_item_show', ['id' => $item->getId()]);
+            }
+
+        return $this->render('item/item_form.html.twig', [
+            'title' => 'Modifier un objet',
+            'item' => $item,
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/admin/item/{id}/delete', name: 'app_admin_item_delete')]
+    public function delete(Item $item, EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $em->remove($item);
+        $em->flush();
+
+        return $this->redirectToRoute('app_item');
     }
 }
